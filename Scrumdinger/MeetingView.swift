84ac508492c52e4fce5e39581e4a76b911a24769
JojoDom <adwoa_dadson@yahoo.com>
2023-04-17 13:44:31 +0000

    ui layout

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -11,7 +11,25 @@ struct MeetingView: View {
     var body: some View {
         VStack {
             ProgressView(value: 5, total: 15)
-        }
+            HStack{
+                VStack ( alignment: .leading){
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.filled")
+                }
+                Spacer()
+                VStack ( alignment: .trailing){
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.filled")
+                }
+            }
+            Circle().strokeBorder(lineWidth: 24)
+            HStack(){
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}){
+                    Image(systemName: "forward.fill")}
+            }
+        }.padding()
     }
 }
 
