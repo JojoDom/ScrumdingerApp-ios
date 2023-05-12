4f02076f2845622e2eb71aecd75e9b0e55f555ef
JojoDom <adwoa_dadson@yahoo.com>
2023-05-12 07:25:14 +0000

    fix

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Adwoa Dadson on 12/05/2023.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
